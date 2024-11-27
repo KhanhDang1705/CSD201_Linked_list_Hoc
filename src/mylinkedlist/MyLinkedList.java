38d791eaf68298bf7635ffc01328d89bae5aca27
KhanhDang <emailcuadang1705@gmail.com>
2024-11-27 22:42:10 +0700

    "version 1.3"

--- src/mylinkedlist/MyLinkedList.java
+++ src/mylinkedlist/MyLinkedList.java
@@ -38,6 +38,49 @@ public class MyLinkedList {
         }
     }
 
+    public int length(){
+        int count = 0;
+        Node temp = head;
+        while (temp != null){
+            count++;
+            temp = temp.getNext();
+        }
+        return count;
+    }
+
+    public void display(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.getData());
+            if (temp.getNext() != null){
+                System.out.print("->");
+            }
+            temp = temp.getNext();
+        }
+        System.out.println();
+    }
+
+    public void deleteFirst(){
+        if (head != null) {
+            head = head.getNext();
+        }
+    }
+
+    public void deleteLast(){
+        if(head == null){
+            return;
+        }
+        if(head.getNext() == null){
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while (temp.getNext().getNext() != null){
+            temp = temp.getNext();
+        }
+        temp.setNext(null);
+    }
+
     public static void main(String[] args) {
         MyLinkedList linkedList = new MyLinkedList();
 //        linkedList.addFirst(1);
@@ -48,5 +91,19 @@ public class MyLinkedList {
         linkedList.addLast(2);
         linkedList.addLast(3);
         linkedList.add(6,1);
+
+        System.out.println("Do dai cua danh sach: "+ linkedList.length());
+
+        System.out.print("Danh sach duoc hien thi: ");
+        linkedList.display();
+
+        linkedList.deleteFirst();
+        System.out.print("Danh sach da xoa phan tu dau: ");
+        linkedList.display();
+
+        linkedList.deleteLast();
+        System.out.print("Danh sach da xoa phan tu cuoi: ");
+        linkedList.display();
+
     }
 }
