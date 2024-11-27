230574daa5d3ff0caab4e19d986dce00c2794e79
KhanhDang <emailcuadang1705@gmail.com>
2024-11-27 22:02:49 +0700

    "version 1.1"

--- src/mylinkedlist/MyLinkedList.java
+++ src/mylinkedlist/MyLinkedList.java
@@ -9,10 +9,28 @@ public class MyLinkedList {
         head = newNode;
     }
 
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.getNext() != null){
+            temp = temp.getNext();
+        }
+        temp.setNext(newNode);
+    }
+
+
     public static void main(String[] args) {
         MyLinkedList linkedList = new MyLinkedList();
-        linkedList.addFirst(1);
-        linkedList.addFirst(2);
-        linkedList.addFirst(3);
+//        linkedList.addFirst(1);
+//        linkedList.addFirst(2);
+//        linkedList.addFirst(3);
+
+        linkedList.addLast(1);
+        linkedList.addLast(2);
+        linkedList.addLast(3);
     }
 }
